e5cdd2ac6f02a6212a364f1141de85466cba17da
Gareth Mellon <garethmellon@gmail.com>
2019-04-09 11:50:58 +0100

    Added some code in to attempt to reduce page load. Added a basic assert test.

--- test_bandcamp.py
+++ test_bandcamp.py
@@ -2,20 +2,32 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+
 class BandcampTest(unittest.TestCase):
     """ run browser and load bandcamp page """
+
     def setUp(self):
-        self.driver = webdriver.Chrome('drivers/chromedriver.exe')
+        chrome_options = webdriver.ChromeOptions()
+
+        # Add some settings to try and reduce the overhead on opening the page
+        prefs = {
+            "profile.managed_default_content_settings.images": 2}
+        chrome_options.add_experimental_option("prefs", prefs)
+
+        # Set driver to headless mode
+        chrome_options.add_argument("--headless")
+        self.driver = webdriver.Chrome('drivers/chromedriver.exe', options=chrome_options)
+
         self.driver.maximize_window()
         self.driver.get('https://bandcamp.com/')
-        self.driver.implicitly_wait(1)
 
-    def test_basic(self):
-        assert "1" == "2"
+    def test_landing_page(self):
+        self.assertEqual(self.driver.title, "Bandcamp")
 
     def tearDown(self):
         """ driver tear down """
         self.driver.close()
 
-if __name__ =="__main__":
+
+if __name__ == "__main__":
     unittest.main()
