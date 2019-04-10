9e2b732f332ce683f4064cdb940e2d16a3da5729
Gareth Mellon <garethmellon@gmail.com>
2019-04-10 16:15:36 +0100

    Added some landed page tests

--- test_bandcamp.py
+++ test_bandcamp.py
@@ -1,12 +1,13 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
 class BandcampTest(unittest.TestCase):
-    """ run browser and load bandcamp page """
 
     def setUp(self):
+        """ run browser and load bandcamp page """
         chrome_options = webdriver.ChromeOptions()
 
         # Add some settings to try and reduce the overhead on opening the page
@@ -22,7 +23,19 @@ class BandcampTest(unittest.TestCase):
         self.driver.get('https://bandcamp.com/')
 
     def test_landing_page(self):
-        self.assertEqual(self.driver.title, "Bandcamp")
+        """ tested on landing page to check if it is valid """
+        try:
+            self.assertEqual(self.driver.title, "Bandcamp")  # assert page title matches
+            assert self.driver.find_element_by_link_text('log in').get_attribute(
+                'href') == "https://bandcamp.com/login?from=home"  # assert login link is correct
+            c = self.driver.find_element_by_css_selector('.carousel-big-item').find_element(By.TAG_NAME,
+                                                                                            "a").get_attribute("href")
+            self.assertEqual(c,
+                             'https://daily.bandcamp.com/2019/04/10/third-man-comes-to-bandcamp-and-the-raconteurs-share-new-song/')  # assert that a link held as a chile of am element is present
+            self.assertTrue(self.driver.find_element_by_css_selector('.corp-bclogo').find_element(By.TAG_NAME,
+                                                                                                  "svg"))  # assert that an svg tag is there within a class
+        except AssertionError:
+            raise AssertionError
 
     def tearDown(self):
         """ driver tear down """
